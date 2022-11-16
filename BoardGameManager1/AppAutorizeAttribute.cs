392e16442fcafb22e8be3977eef42178eb1f80a3
6ye6ye <guzova.elizaveta00@gmail.com>
2022-11-16 13:31:38 +0600

    Add AppAutorizeAttribute.cs

---
+++ BoardGameManager1/AppAutorizeAttribute.cs
@@ -0,0 +1,14 @@
+﻿using BoardGameManager1.Entities;
+using BoardGameManager1.Enums;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BoardGameManager1
+{
+    public class AppAutorizeAttribute:AuthorizeAttribute
+    {
+        public AppAutorizeAttribute(params UserRoleEnum[] roles):base()
+        { 
+            Roles = string.Join(",", roles.Select(r => r));
+        }
+    }
+}
